340beceaee727222ce6f6854e28ef2043c505c34
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-09-28 11:14:26 -0700

    update

---
+++ hello_gradescope.py
@@ -0,0 +1,9 @@
+# Name: Christopher boyd
+# OSU Email: boydc3@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: hello_gradescope.py
+# Due Date: 10/3/22
+# Description: test
+
+def gradescope_test():
+    return 42
